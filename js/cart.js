fb8efbbb01b91d4275c45bb54d2beaf579abca20
Dino-Nguyen <xiao.dinnie@gmail.com>
2022-02-15 07:29:19 +0700

    cm1

--- js/cart.js
+++ js/cart.js
@@ -29,9 +29,138 @@ userContainer.innerHTML += `
 // RENDER MAIN BODY OF CART
 mainPage.innerHTML += `
     <div id="main__container">
-    MAIN
+   
+    <div class="modal-body">
+    <div class="cart-row">
+        <span class="cart-item cart-header cart-column">Product</span>
+        <span class="cart-price cart-header cart-column">Price</span>
+        <span class="cart-quantity cart-header cart-column">Quantity</span>
     </div>
-`
+    <div class="cart-items">
+        <div class="cart-row">
+       
+    </div>
+   
+  
+    <div class="cart-total">
+        <strong class="cart-total-title">Total Price:</strong>
+        <span class="cart-total-price"></span>
+    </div>
+</div>
+
+
+    </div>`
+
+// RUN FUNCTIONS 
+// ------------------------------------------------------------------
+
+// nut Xoa items
+let remove_cart = document.getElementsByClassName("btn-danger");
+for (let i = 0; i < remove_cart.length; i++) {
+  let button = remove_cart[i]
+  button.addEventListener("click", function () {
+    let button_remove = event.target
+    button_remove.parentElement.parentElement.remove()
+    updatecart()
+  }) 
+}
+
+// total price
+function updatecart() {
+    let cart_item = document.getElementsByClassName("cart-items")[0];
+    let cart_rows = cart_item.getElementsByClassName("cart-row");
+    let total = 0;
+    for (let i = 0; i < cart_rows.length; i++) {
+      let cart_row = cart_rows[i]
+      let price_item = cart_row.getElementsByClassName("cart-price")[0]
+      let quantity_item = cart_row.getElementsByClassName("cart-quantity-input")[0]
+      let price = parseFloat(price_item.innerText)
+      let quantity = quantity_item.value 
+      total = total + (price * quantity)
+    }
+    document.getElementsByClassName("cart-total-price")[0].innerText = '$' + total
+    
+  }
+
+  // thay doi so luong sp
+  let quantity_input = document.getElementsByClassName("cart-quantity-input");
+  for (let i = 0; i < quantity_input.length; i++) {
+    let input = quantity_input[i];
+    input.addEventListener("change", function (event) {
+      let input = event.target
+      if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1;
+      }
+      updatecart()
+    })
+  }
+
+// thêm sp vào giỏ hàng ,,, lấy giá trị từ local xuống thay vào để render
+  let add_cart = document.getElementsByClassName("btn-cart");
+for (let i = 0; i < add_cart.length; i++) {
+  let add = add_cart[i];
+  add.addEventListener("click", function (event) {
+
+    let button = event.target;
+    let product = button.parentElement.parentElement;
+    let img = product.parentElement.getElementsByClassName("img-prd")[0].src
+    let title = product.getElementsByClassName("content-product-h3")[0].innerText
+    let price = product.getElementsByClassName("price")[0].innerText
+    addItemToCart(title, price, img)
+   
+    modal.style.display = "block";
+    
+    updatecart()
+  })
+}
+
+function addItemToCart(title, price, img) {
+  var cartRow = document.createElement('div')
+  cartRow.classList.add('cart-row')
+  var cartItems = document.getElementsByClassName('cart-items')[0]
+  var cart_title = cartItems.getElementsByClassName('cart-item-title')
+  
+  for (var i = 0; i < cart_title.length; i++) {
+    if (cart_title[i].innerText == title) {
+        input.value + 1
+
+
+
+      return
+    }
+  }
+
+  var cartRowContents = `
+  <div class="cart-item cart-column">
+      <img class="cart-item-image" src="${img}" width="100" height="100">
+      <span class="cart-item-title">${title}</span>
+  </div>
+  <span class="cart-price cart-column">${price}</span>
+  <div class="cart-quantity cart-column">
+      <input class="cart-quantity-input" type="number" value="1">
+      <button class="btn btn-danger" type="button">Xóa</button>
+  </div>`
+  cartRow.innerHTML = cartRowContents
+  cartItems.append(cartRow)
+  cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', function () {
+    var button_remove = event.target
+    button_remove.parentElement.parentElement.remove()
+    updatecart()
+  })
+  cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', function (event) {
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+      input.value = 1;
+    }
+    updatecart()
+  })
+}
+
+
+let productNumbers = localStorage.getItem('cartNumbers');
+document.querySelector('.cart__number').textContent = productNumbers;
+
+// ------------------------------------------------------------------
 
 
 // RENDER FOOTER
@@ -101,9 +230,5 @@ function renderFooter2() {
 }
 renderFooter2()
 
-// FUNCTION SHOW HOW CART NUMBERS AT LOCAL STORAGE
-function keep_status_of_cart() {
-    let productNumbers = localStorage.getItem('cartNumbers');
-    document.querySelector('.cart__number').textContent = productNumbers;
-}
-keep_status_of_cart();
+
+
